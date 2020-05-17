81efdc11ffed37679dee66732ad778a35bbd363f
Sergio D. Barrera <sdbeng@gmail.com>
2020-05-17 12:13:32 -0700

    polish post title and date

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -2,6 +2,7 @@ import Layout from "../../components/layout"
 import {getAllPostIds, getPostData} from '../../lib/posts'
 import Head from "next/head"
 import Date from "../../components/date"
+import utilStyles from '../../styles/utils.module.css'
 
 const Post = ({postData}) => {
     return (
@@ -9,11 +10,15 @@ const Post = ({postData}) => {
             <Head>
                 <title>{postData.title}</title>
             </Head>
-            {postData.title}
-            <br />
-            <Date dateString={postData.date} />            
+            <article>
+            <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+            <div className={utilStyles.lightText}>
+            <Date dateString={postData.date} />          
+            </div>
             <br />
             <div dangerouslySetInnerHTML={{__html: postData.contentHtml}}></div>
+
+            </article>
         </Layout>
     )
 }
