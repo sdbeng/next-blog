384902c4191806ec28ca47a609c2d7551ab8aaf1
Sergio D. Barrera <sdbeng@gmail.com>
2020-05-16 22:53:14 -0700

    create _app.js to add  global css

---
+++ pages/_app.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../styles/global.css'
+
+const App = ({ Component, pageProps }) => {
+    return (
+        <Component {...pageProps} />
+            
+        
+    )
+}
+
+export default App
