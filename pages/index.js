84e27b9e1cda1f8f30470cf781c4fff3cd6378b7
Sergio D. Barrera <sdbeng@gmail.com>
2020-05-17 09:36:03 -0700

    install gray-matter, create markdwn posts, render

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,47 @@
 import Head from 'next/head'
 import Link from 'next/link'
 import Layout, { siteTitle } from '../components/layout'
+import { getSortedPostsData } from '../lib/posts'
+import utilStyles from '../styles/utils.module.css'
 
-export default function Home() {
+export async function getStaticProps(){
+  const allPostsData = getSortedPostsData()
+  return {
+    props:{
+      allPostsData
+    }
+  }
+}
+
+export default function Home({ allPostsData }) {
+
   return (
-    <Layout>
+    <Layout home>
       <Head>
         <title>{siteTitle}</title>
       </Head>
       <section>
         <p>Hello, I'm Serg ⚽️</p>
         <p>I'm a Full Stack Developer living in Concord,CA. I develop custom web and mobile apps for business and non-profit organizations. Contact me on Twitter at <span><a>sergdb_dev</a></span> </p>
+      </section>
+      <section className={utilStyles.headingMd}>
+
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+        {
+          allPostsData.map(({id, date, title}) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))
+        }
+        </ul>
       </section>
     </Layout>
   )
