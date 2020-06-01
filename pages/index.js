3dbd46122e3fb87fd7f537e8979119ee6b58a81a
Sergio D. Barrera <sdbeng@gmail.com>
2020-06-01 08:53:37 -0700

    add image

--- pages/index.js
+++ pages/index.js
@@ -26,11 +26,16 @@ export default function Home({ allPostsData }) {
         <p>I'm a Full Stack Developer living in Concord,CA. I develop custom web and mobile apps for business and non-profit organizations. </p>
         <p>On my free time, I like bike riding, swimming and playing chess with my son. 
         Contact me on Twitter at <span><a href="https://twitter.com/sergb_appdev">sergb_appdev</a></span> </p>
+        
       </section>
+      <div>
+      <img className="mt-6 rounded-lg shadow-xl sm:mt-8 sm:h-64 sm:w-full sm:object-cover sm:object-center lg:hidden" src="/images/summer-beach.jpg" alt="on the beach" />
+      </div>
+
       <section className={utilStyles.headingMd}>
 
       </section>
-      <section bg-indigo-200 shadow>
+      <section>
         <h2 className="m-4 h-6 text-2xl text-indigo-900">Blog</h2>
         <ul className="list-none">
         {
