5b694b8518e849a3e62aeed7ec5377f7249f1500
Sergio D. Barrera <sdbeng@gmail.com>
2020-06-06 01:41:45 -0700

    add game page, gamelogic comp

---
+++ components/gamelogic.js
@@ -0,0 +1,19 @@
+
+
+const gamelogic = (props) => {
+    return (
+        <div class="bg-gray-200 flex">
+            <div class="px-8 py-12 max-w-md mx-auto sm:max-w-xl lg:max-w-full lg:w-1/2 lg:py-24 lg:px-12">
+                <div class="xl:max-w-lg xl:ml-auto">
+                <h1 class="mt-6 text-2xl font-bold text-gray-900 leading-tight sm:mt-8 sm:text-4xl lg:text-3xl xl:text-4xl">
+                Game strategies
+                <br class=" lg:inline" /><span class="text-xl text-indigo-500">Read the rules of the game.</span>
+                </h1>
+                </div>
+            </div>
+            
+        </div>
+    )
+}
+
+export default gamelogic
